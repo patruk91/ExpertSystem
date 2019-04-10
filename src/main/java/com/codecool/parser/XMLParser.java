c021b2e05fd9afc78b2f392826728ac94380fbe3
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-04-10 17:56:31 +0200

    Load and parse xml document

--- src/main/java/com/codecool/parser/XMLParser.java
+++ src/main/java/com/codecool/parser/XMLParser.java
@@ -1,4 +1,26 @@
 package com.codecool.parser;
 
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
 public abstract class XMLParser {
+    Document doc;
+
+    void loadXmlDocument(String xmlPath) {
+        try {
+            File fileToRead = new File(xmlPath);
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            documentBuilder.parse(fileToRead);
+            doc.getDocumentElement().normalize();
+        } catch (ParserConfigurationException | IOException | SAXException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
