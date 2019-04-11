b8ff756140beb2bb9ae1e811c4ae49333b9e1007
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-04-11 08:39:36 +0200

    Invoke rule repository class in rule parser class

--- src/main/java/com/codecool/parser/RuleParser.java
+++ src/main/java/com/codecool/parser/RuleParser.java
@@ -1,4 +1,20 @@
 package com.codecool.parser;
 
-public class RuleParser {
+import com.codecool.repository.RuleRepository;
+
+public class RuleParser extends XMLParser {
+    RuleRepository ruleRepository;
+
+    public RuleParser() {
+        this.ruleRepository = new RuleRepository();
+        loadXmlDocument("src/main/resources/rules.xml");
+    }
+
+    public RuleRepository getRuleRepository() {
+        return ruleRepository;
+
+    }
+
+
+
 }
