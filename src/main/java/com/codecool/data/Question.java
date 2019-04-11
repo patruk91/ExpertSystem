4bd1c216b6166610e9333469cc9011f003c07169
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-04-11 08:40:10 +0200

    Implement Question class, and method getId, getQuestion

--- src/main/java/com/codecool/data/Question.java
+++ src/main/java/com/codecool/data/Question.java
@@ -1,4 +1,21 @@
 package com.codecool.data;
 
 public class Question {
+    private String id;
+    private String question;
+    private Answer answer;
+
+    public Question(String id, String question, Answer answer) {
+        this.id = id;
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
 }
