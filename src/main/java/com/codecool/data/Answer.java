01e421cf6abb0594a597223f1f1540831e673fd5
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-04-11 09:16:33 +0200

    Add method to compare user input with values

--- src/main/java/com/codecool/data/Answer.java
+++ src/main/java/com/codecool/data/Answer.java
@@ -1,4 +1,29 @@
 package com.codecool.data;
 
+import com.codecool.value.Value;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Answer {
+    private List<Value> values;
+
+    public Answer() {
+        this.values = new ArrayList<>();
+    }
+
+    public boolean evaluateAnswerByInput(String input) {
+        for (Value value : values) {
+            for (String element : value.getInputPattern()) {
+                if (element.equals(input)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void addValue(Value value) {
+        values.add(value);
+    }
 }
