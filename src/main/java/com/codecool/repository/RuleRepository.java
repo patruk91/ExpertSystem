2186d7d1cdf6c84f841957fa23a8ee8da442c75f
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-04-11 08:38:52 +0200

    Add rule repository class, implement iterator interface

--- src/main/java/com/codecool/repository/RuleRepository.java
+++ src/main/java/com/codecool/repository/RuleRepository.java
@@ -1,4 +1,46 @@
 package com.codecool.repository;
 
+import com.codecool.data.Question;
+
+import java.util.*;
+
 public class RuleRepository {
+    private Map<String, Question> questions;
+    private Iterator<Question> iterator;
+
+    public RuleRepository() {
+        this.questions = new LinkedHashMap<>();
+        this.iterator = new QuestionIterator();
+    }
+
+    public void addQuestion(Question question) {
+        questions.put(question.getId(), question);
+    }
+
+    public Iterator<Question> getIterator() {
+        return iterator;
+    }
+
+    private class QuestionIterator implements Iterator<Question> {
+        private int index = 0;
+        @Override
+        public boolean hasNext() {
+            return index < questions.size();
+        }
+
+        @Override
+        public Question next() {
+            if (!this.hasNext()) {
+                throw new NoSuchElementException();
+                // or return null
+            }
+//            (Question) questions.values().toArray()[index++];
+            index++;
+            String keyId = questions.keySet().iterator().next();
+            return questions.get(keyId);
+
+
+        }
+    }
+
 }
