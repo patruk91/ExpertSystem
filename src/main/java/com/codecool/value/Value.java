1b7a18328130881f7fcec482378f8d6b99ed435d
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-04-11 09:09:13 +0200

    Add method to select type, and to get input pattern

--- src/main/java/com/codecool/value/Value.java
+++ src/main/java/com/codecool/value/Value.java
@@ -1,4 +1,18 @@
 package com.codecool.value;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Value {
+    boolean selectionType;
+    List<String> patterns = new ArrayList<>();
+
+    protected boolean getSelectionType() {
+        return selectionType;
+    }
+
+    protected List<String> getInputPattern() {
+        return patterns;
+    }
+
 }
