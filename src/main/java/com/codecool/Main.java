577acc97eb3e7006f559b91979afde30fb35bbfa
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-04-12 08:39:52 +0200

    Add main method, and methods in ESProvider class

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -1,4 +1,16 @@
 package com.codecool;
 
+import com.codecool.parser.FactParser;
+import com.codecool.provider.ESProvider;
+import com.codecool.repository.FactRepository;
+
 public class Main {
+    public static void main(String[] args) {
+        FactParser factParser = new FactParser();
+        FactRepository factRepository = new FactRepository();
+        ESProvider esProvider = new ESProvider(factParser, factRepository);
+
+        esProvider.collectAnswers();
+
+    }
 }
--- src/main/java/com/codecool/provider/ESProvider.java
+++ src/main/java/com/codecool/provider/ESProvider.java
@@ -1,4 +1,26 @@
 package com.codecool.provider;
 
+import com.codecool.parser.FactParser;
+import com.codecool.repository.FactRepository;
+
 public class ESProvider {
+    private FactParser factParser;
+    private FactRepository factRepository;
+
+    public ESProvider(FactParser factParser, FactRepository factRepository) {
+        this.factParser = factParser;
+        this.factRepository = factRepository;
+    }
+
+    public void collectAnswers() {
+
+    }
+
+    public boolean getAnswerByQuestion(String questionId) {
+        return false;
+    }
+
+    public String evaluate() {
+        return "";
+    }
 }
