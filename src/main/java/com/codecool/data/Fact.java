ed2a544183b7ddf5fd412028b0db4304e9848317
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-04-12 06:33:32 +0200

    Add methods to fact class, getters and setters

--- src/main/java/com/codecool/data/Fact.java
+++ src/main/java/com/codecool/data/Fact.java
@@ -1,4 +1,39 @@
 package com.codecool.data;
 
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Fact {
+    private String id;
+    private Boolean  description;
+    private Map<String, Boolean> facts;
+
+    public Fact(String id, Boolean description) {
+        this.id = id;
+        this.description = description;
+        this.facts = new LinkedHashMap<>();
+    }
+
+    public Set<String> getIdSet() {
+        Set<String> idSet = new LinkedHashSet<>();
+        for (String keys : facts.keySet()) {
+            idSet.add(keys);
+        }
+        return idSet;
+    }
+
+    public void setFactValueById(String id, boolean value) {
+        facts.put(id, value);
+    }
+
+
+    public Boolean getValueById(String id) {
+        return facts.get(id);
+    }
+
+    public Boolean getDescription() {
+        return description;
+    }
 }
