0fd1c13c8a34619dc33ff18f403e48c1558add00
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-04-12 06:40:44 +0200

    Add methods into fact parser class, getters, and to parse a file

--- src/main/java/com/codecool/parser/FactParser.java
+++ src/main/java/com/codecool/parser/FactParser.java
@@ -1,4 +1,22 @@
 package com.codecool.parser;
 
-public class FactParser {
+import com.codecool.repository.FactRepository;
+import org.w3c.dom.Document;
+
+public class FactParser extends  XMLParser{
+    private FactRepository factRepository;
+
+    public FactParser() {
+        this.factRepository = new FactRepository();
+        loadXmlDocument("src/main/resources/facts.xml");
+        parseFacts(getDoc());
+    }
+
+    public FactRepository getFactRepository() {
+        return factRepository;
+    }
+
+    private void parseFacts(Document document) {
+        
+    }
 }
