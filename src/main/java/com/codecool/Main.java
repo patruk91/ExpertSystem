0dc3ebb78f1a58abbdd8a8f63fce950c578c3bf2
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-04-14 08:40:32 +0200

    Add main function to run program

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -3,15 +3,16 @@ package com.codecool;
 import com.codecool.parser.FactParser;
 import com.codecool.parser.RuleParser;
 import com.codecool.provider.ESProvider;
-import com.codecool.repository.FactRepository;
 
 public class Main {
     public static void main(String[] args) {
-        RuleParser ruleParser = new FactParser();
-        FactRepository factRepository = new FactRepository();
-        ESProvider esProvider = new ESProvider(ruleParser, factRepository);
+        RuleParser ruleParser = new RuleParser();
+        FactParser factParser = new FactParser();
+        ESProvider esProvider = new ESProvider(ruleParser, factParser);
 
         esProvider.collectAnswers();
+        System.out.println("Suggested game: ");
+        System.out.print(esProvider.evaluate());
 
     }
 }
